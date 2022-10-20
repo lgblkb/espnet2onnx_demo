0db42026224d17425e6950064c4140a94f569424
lgblkb <dbakhtiyarov@nu.edu.kz>
2022-10-20 07:59:30 +0600

    Done

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 import io
 
+import requests
 from loguru import logger
 import re
 from typing import List
@@ -37,17 +38,7 @@ response_example = {
 
 @app.get("/data", response_model=Response, responses=response_example)
 def get_result(audio_file: str):
-    # fd = io.BytesIO()
-    # with open(fd,'wb') as file:
-    #     write(fd, fs, audio_data)
-    # return base64.b64encode(fd.read())
-    logger.debug('audio_file: {}', audio_file)
-    text = convert_speech_to_text(audio_file)
-    logger.debug('text: {}', text)
-
-    # # Print tokens and predicted labels
-    # if model_name == "roberta":
-    #     input_sent_tokens = tokenizer.decode(tokenized_inputs['input_ids'][0], skip_special_tokens=True).split()
-    # else:
-
+    response = requests.get(audio_file)
+    fd = io.BytesIO(response.content)
+    text = convert_speech_to_text(fd)
     return dict(text=text)
--- app/utils.py
+++ app/utils.py
@@ -3,14 +3,20 @@ from pathlib import Path
 
 import librosa
 from espnet_onnx import Speech2Text
+from loguru import logger
 
 data_folder = Path(os.environ['STORAGE_FOLDER'])
-model_dir = data_folder / 'train_asr_ksc_v1'
-assert model_dir.exists()
-speech2text = Speech2Text(model_dir=model_dir, use_quantized=True)
+logger.debug('data_folder: {}', data_folder)
+# model_dir = data_folder / 'train_asr_ksc_v1'
+# assert model_dir.exists()
+# logger.info('model_dir: {}', model_dir)
+curdir = os.curdir
+os.chdir(data_folder)
+speech2text = Speech2Text(model_dir='train_asr_ksc_v1', use_quantized=True)
+os.chdir(curdir)
 
 
-def convert_speech_to_text(wav_file: str):
+def convert_speech_to_text(wav_file):
     y, sr = librosa.load(wav_file, sr=16000)
     text = speech2text(y)[0][0]
     return text
